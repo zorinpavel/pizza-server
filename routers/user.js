936b721b1ec93ad517348bcc68989d173cd7ea97
pavelzorin <zorin.pavel@gmail.com>
2020-09-22 01:38:20 +0300

    Signup

--- routers/user.js
+++ routers/user.js
@@ -9,30 +9,23 @@ router.options('/*', (req, res) => {
     return res.status(200);
 });
 
-router.get('/', async(req, res) => {
+router.get('/', auth, async(req, res) => {
     return res.status(200).send({ user: req.user });
 });
 
 
-router.get('/:us_id', auth, async(req, res) => {
-    const us_id = req.params.us_id;
+router.get('/:id', auth, async(req, res) => {
+    const _id = req.params.id;
 
     try {
         const user = await User
-            .findOne({ us_id })
-            .populate({
-                path: 'contacts',
-                populate: {
-                    path: 'user',
-                }
-            });
+            .findOne({ _id });
 
         if(!user)
             return res.status(404).send({ error: 'Cant find user' });
 
         return res.send({
-            ...user.toJSON(),
-            contacts: user.contacts
+            ...user.toJSON()
         });
     } catch(e) {
         return res.status(400).send({ error: 'Cant get user', message: e.message });
@@ -45,7 +38,7 @@ router.post('/login', async(req, res) => {
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const authToken = await user.generateAuthToken();
 
-        return res.send({ user, authToken });
+        return res.status(200).send({ user, authToken });
     } catch(e) {
         return res.status(400).send({ error: 'Request is not valid: provide valid user', message: e.message });
     }
@@ -57,7 +50,27 @@ router.post('/logout', auth, async(req, res) => {
         req.user.authToken = '';
         await req.user.save();
 
-        return res.status(200).send({});
+        return res.status(200);
+    } catch(e) {
+        return res.status(500).send({ error: 'Bad request', message: e.message });
+    }
+});
+
+
+router.post('/signup', async(req, res) => {
+    try {
+        const userRequested = { name, email, address } = req.body;
+
+        const user = await User.findOne({ email: req.body.email })
+            .then((model = {}) => {
+                return model ? Object.assign(model, userRequested) : new User(userRequested);
+            })
+            .then(model => model.save())
+            .catch(e => {
+                throw new Error(e);
+            });
+
+        return res.status(200).send({ user, authToken: user.authToken });
     } catch(e) {
         return res.status(500).send({ error: 'Bad request', message: e.message });
     }
